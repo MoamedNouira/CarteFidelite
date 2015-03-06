4505cbfa267a33db40219f7a06d198e97c1bedf5
Mohamed Nouira <gnouira@gmail.com>
2015-03-07 00:26:13 +0100

    ccc

--- app/src/main/java/com/medbac/carte_fidelite/activity/Menu_Carte.java
+++ app/src/main/java/com/medbac/carte_fidelite/activity/Menu_Carte.java
@@ -11,25 +11,15 @@ import activity.carte_fidelite.medbac.com.cartefidelite.R;
 /**
  * Created by Mohamed Nouira on 06/03/2015.
  */
-public class Login  extends Activity {
+public class Menu_Carte  extends Activity {
 
-    Button creer_compte;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-        creer_compte = (Button) findViewById(R.id.button3);
-    }
-
-    public void onClick(View v) {
-        if (v.getId() == creer_compte.getId()) {
-            Intent i = new Intent(this, Creer_compte.class);
-            startActivity(i);
-
-
-        }
+        setContentView(R.layout.menu_carte);
 
     }
+
 }
