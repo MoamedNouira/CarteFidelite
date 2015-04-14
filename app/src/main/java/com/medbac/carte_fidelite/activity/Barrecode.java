7a86ee5103696fed0c4e9d6f32e216630178c352
Mohamed Nouira <gnouira@gmail.com>
2015-04-14 01:36:06 +0100

    get de donne de basse

--- app/src/main/java/com/medbac/carte_fidelite/activity/Barrecode.java
+++ app/src/main/java/com/medbac/carte_fidelite/activity/Barrecode.java
@@ -2,12 +2,18 @@ package com.medbac.carte_fidelite.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.Writer;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -17,15 +23,18 @@ import activity.carte_fidelite.medbac.com.cartefidelite.R;
  * Created by Mohamed Nouira on 13/04/2015.
  */
 public class Barrecode extends Activity  implements View.OnClickListener{
-
+    private static final int WHITE = 0xFFFFFFFF;
+    private static final int BLACK = 0xFF000000;
     Button scanBtn,ok;
     EditText code;
+    ImageView icode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barre_code);
         scanBtn = (Button) findViewById(R.id.button);
         ok = (Button) findViewById(R.id.button2);
+        icode = (ImageView) findViewById(R.id.imageView2);
 
         code = (EditText) findViewById(R.id.editText);
         scanBtn.setOnClickListener(this);
@@ -48,6 +57,10 @@ public class Barrecode extends Activity  implements View.OnClickListener{
             // setResult(Facvc.RESULT_OK);
             setResult(RESULT_OK, data);
 
+            Intent i = new Intent(Barrecode.this, AffichageBarreCode.class);
+            i.putExtra("scode",scode);
+            // startActivity(i);
+            startActivity(i);
             finish();
         }
     }
@@ -57,7 +70,9 @@ public class Barrecode extends Activity  implements View.OnClickListener{
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanningResult != null) {
             String scanContent = scanningResult.getContents();
+            String forma=  scanningResult.getFormatName();
             code.setText(scanContent);
+
         }else{
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
@@ -65,7 +80,6 @@ public class Barrecode extends Activity  implements View.OnClickListener{
         }
 
     }
-
 
 
 
